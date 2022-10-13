19e013e90fcabaa2b7e677cc6189c42bed0c8898
BurakTetikk <burktetk@gmail.com>
2022-10-13 23:18:16 +0300

    Exercise

--- Chapters/chapter_07/Exercise01.java
+++ Chapters/chapter_07/Exercise01.java
@@ -12,7 +12,6 @@ public class Exercise01 {
         int[] score = new int[studentsNumber];
         getScore(score, input);
         printScore(score);
-        System.out.println();
     }
 
     public static void printScore(int[] score) {
