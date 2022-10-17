908e4877b3463099a3b4aa29a39e695c5080e7e5
BurakTetikk <burktetk@gmail.com>
2022-10-17 11:03:42 +0300

    Exercise

---
+++ Chapters/chapter_02/CheckPoints/CheckPoint_01.java
@@ -0,0 +1,14 @@
+package Chapters.chapter_02.CheckPoints;
+
+public class CheckPoint_01 {
+    public static void main(String[] args) {
+        //Declare and assign variables
+        double i = 50.0;
+        double k = i + 50.0;
+        double j = k + 1;
+
+        //Display results
+        System.out.println("j is " + j + " and " +
+                "k is " + k);
+    }
+}
---
+++ Chapters/chapter_02/Listings/Listing_01.java
@@ -0,0 +1,20 @@
+package Chapters.chapter_02.Listings;
+
+public class Listing_01 {
+    public static void main(String[] args) {
+        //Declare radius
+        double radius;
+
+        //Declare area
+        double area;
+
+        //Assign a radius
+        radius = 20;
+
+        //Compute area
+        area = radius * radius * 3.14159;
+
+        //Display results
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+    }
+}
